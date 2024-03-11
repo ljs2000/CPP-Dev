be8a2fbbc7a583102af84bd8fcaffb8fc1354ac0
Lucas Serafin <lucasserafin21@gmail.com>
2024-03-11 16:54:28 -0400

    Update SimpleCalculator.cpp

--- SimpleCalculator.cpp
+++ SimpleCalculator.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int main() 
+int main()
 {
 
 int input;
@@ -15,15 +15,15 @@ int sum;
 
 cout << "Simple Calculator" << endl;
 
-cout << "Enter a number" << endl;
+cout << "Enter a number:" << endl;
 
 cin >> x;
 
-cout << "Enter another number" << endl;
+cout << "Enter another number:" << endl;
 
 cin >> y;
 
-cout << "Do you want to add, subtract, multiply or divide these numbers?" << endl;
+cout << "Do you want to add, subtract, multiply or divide these two numbers?" << endl;
 
 cout << "Enter 1 for addition, 2 for subtraction, 3 for multiplication and 4 for division:" << endl;
 
@@ -34,7 +34,25 @@ if(input == 1)
 sum = x + y;
 cout << sum << endl;
 }
-
-
-
+else if(input == 2)
+{
+sum = x - y;
+}
+else if(input == 3)
+{
+sum = x * y;
+}
+else if(input == 4)
+{
+    sum = x / y;
+}
+else if(input >= 4)
+{
+cout << "Please enter a number between 1-4.\n";
+}
+else if(input <=1)
+{
+cout << "Please enter a number between 1-4.\n";
+}
+return 0;
 }
