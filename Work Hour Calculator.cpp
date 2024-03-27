8ed0800276715187e2fc731d61243283924479f0
Lucas Serafin <lucasserafin21@gmail.com>
2024-03-26 20:14:42 -0400

    Update Work Hour Calculator.cpp

--- Work Hour Calculator.cpp
+++ Work Hour Calculator.cpp
@@ -7,10 +7,12 @@ int main()
 int credits;
 int sum;
 int choice;
+int days;
+int sum2;
     cout << "Enter number of credits:\n";
     cin >> credits;
-    cout << "The general recommendation is 2-3 hours per credit.\n";
     cout << "Do you want to study 2 or 3 hours per credit? Enter 2 or 3: \n";
+    cout << "The general recommendation is 2-3 hours per credit.\n";
     cin >> choice;
     if (choice == 2)
     {
@@ -22,5 +24,9 @@ int choice;
     sum = credits * 3;
     cout << "You should be studying " << sum << " hours per week\n";
     }
+    cout << "How many days do you want to study per week?\n";
+    cin >> days;
+    sum2 = sum / days;
+    cout << "Then you should be studying " << sum2 << " hours a day.\n";
     return 0;
 }
